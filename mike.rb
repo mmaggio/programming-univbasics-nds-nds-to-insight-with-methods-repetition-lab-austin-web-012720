fe0c2aa4e3e6bc75ec1cde1d593b7efd9f6556ed
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-13 02:02:14 +0000

    Done.

--- mike.rb
+++ mike.rb
@@ -1,6 +1,17 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
+def gross_for_director(d)
+  total = 0
+  index = 0
+
+  while index < d[:movies].length do
+    total += d[:movies][index][:worldwide_gross]
+    index += 1
+  end
+  total
+end
+
 def directors_totals(source)
   result = {}
   director_index = 0
@@ -10,18 +21,6 @@ def directors_totals(source)
     director_index += 1
   end
   result
-end
-
-def gross_for_director(d)
-  total = 0
-  index = 0
-
-  while index < d[:movies].length do
-    total += d[:movies][index][:worldwide_gross]
-    index += 1
-  end
-
-  total
 end
 
 def list_of_directors(source)
@@ -34,21 +33,11 @@ def list_of_directors(source)
   director_list
 end
 
-
 def total_gross(source)
-  # Write this implementation
-  #
-  # Should use methods:
-  # 1. directors_totals: returns a Hash of { dir_name => gross }
-  # 2. list_of_directors: names provides an Array of directors names (use
-  #
-  # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
-  # return the total
+  
   key_name = []
   hash_totals = {}
   index = 0
-  total = 0
   key_name = list_of_directors(source)
   hash_totals = directors_totals(source)
   
@@ -58,5 +47,3 @@ def total_gross(source)
   end
   total
 end
-
-
